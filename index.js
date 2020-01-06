421280aede9f4df942d20e7bc2045cd5d06051e4
shams khalid <shamsnaji4@gmail.com>
2020-01-06 18:21:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers=function (drivers){
-  return 
+  return drivers.slice(0,2)
 }
 
 const returnLastTwoDrivers= function(drivers){
-  return drivers.slice
+  return drivers.slice(-2)
 }
+
+
